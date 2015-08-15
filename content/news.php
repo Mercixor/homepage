a0e2d699dc3047436b94d6b32d149bf34aee56d4
Mercixor <mercixor@users.noreply.github.com>
2015-08-15 12:57:34 +0200

    REFACTOR: Dateipfade angepasst

---
+++ content/news.php
@@ -0,0 +1,7 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mercix
+ * Date: 06.08.2015
+ * Time: 21:46
+ */
---
+++ libs/module.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Mercix
+ * Date: 06.08.2015
+ * Time: 21:39
+ */
+
+namespace content;
+
+
+class module
+{
+
+}
